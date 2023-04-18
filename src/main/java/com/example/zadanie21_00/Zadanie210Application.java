18690732b399f779250a52da3646360abe123d18
Alina Burnos <burnosalina@gmail.com>
2023-04-18 07:27:09 +0200

    zadanie21.0

---
+++ src/main/java/com/example/zadanie21_00/Zadanie210Application.java
@@ -0,0 +1,13 @@
+package com.example.zadanie21_00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie210Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie210Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zadanie21_00/Zadanie210ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadanie21_00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie210ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
